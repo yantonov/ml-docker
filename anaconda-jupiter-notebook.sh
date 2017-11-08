3983b0734c67fee83a4bbd30a39b959202a9e788
Yury Antonov <yantonov@yandex.ru>
2017-11-09 00:02:46 +0500

    its possible to use anaconda image with external repos

--- anaconda-jupiter-notebook.sh
+++ anaconda-jupiter-notebook.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-docker run -i -t -p 8888:8888 continuumio/anaconda3 /bin/bash -c "useradd dev --create-home && mkdir /opt/notebooks && chown dev:dev /opt/notebooks && su - dev -c \"/opt/conda/bin/conda install jupyter -y --quiet && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser\""
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+cd $SCRIPT_DIR
+
+SCRIPT=`basename $0`
+
+if [ "$#" -lt 1 ]; then
+    echo "usage: $SCRIPT <notebook_dir>"
+    exit 0
+fi
+
+NOTEBOOK_LOCAL_DIR=$1
+
+docker run -i -t \
+       -p 8888:8888 \
+       -v $NOTEBOOK_LOCAL_DIR:/opt/notebooks \
+       continuumio/anaconda3 \
+       /bin/bash -c \
+       "useradd dev --create-home \
+       && su - dev -c \"/opt/conda/bin/conda install jupyter -y --quiet \
+       && /opt/conda/bin/jupyter notebook \
+       --notebook-dir=/opt/notebooks \
+       --ip='*' \
+       --port=8888 \
+       --no-browser\""
