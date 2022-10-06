09b2c66d62d9849d9083479b42acfce01a631939
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2022-10-06 14:02:39 +0200

    fix find-outdated script

--- docker/files/find-outdated-packages.sh
+++ docker/files/find-outdated-packages.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env sh
 
-pip install piprot
-
 echo 'List of packages to update:'
+
+
+echo "Check pip list"
+pip list -o --format=freeze | grep '==' | awk -F== '{ print $1 }' | sort > tmp_pip_list
+
+echo "Check requirements.txt"
+awk -F== '{ print $1 }' requirements.txt | sort > tmp_req_list
+
 echo '---'
-piprot /requirements.txt | grep -v 'up to date'
+join tmp_pip_list tmp_req_list
 echo '---'
+
+rm tmp_pip_list
+rm tmp_req_list
+
+echo "Latest versions are here:"
+pip list --outdated
